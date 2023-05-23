dfdf9a123a73f30d4c49b657d07e4ba9ca66d932
BT_3W2_11 <timurb2@nycstudents.net>
2023-05-23 09:28:46 -0400

    finished most of the classes and can return the coordiates of the vertex

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Enter in an equation please.  Format : a(x-h)^2+k).  No spaces please.");
+        String x = scan.nextLine();
+        Vertex v1 = new Vertex(x);
+        v1.present();
+
+
+    }
+
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -7,19 +7,51 @@ public class Vertex {
 
     public double getX(){
         String temp = formula.substring(0, formula.indexOf("^"));
+        boolean positive = true;
         int firstLoc = 0;
         if(temp.contains("+")){
             firstLoc = temp.indexOf("+");
         }
         else if(!temp.contains("+")){
             firstLoc = temp.indexOf("-");
+            positive = false;
         }
-        return Double.parseDouble(temp.substring((firstLoc + 1), temp.indexOf(")")));
+        if(positive) {
+            return -1 * Double.parseDouble(temp.substring((firstLoc + 1), temp.indexOf(")")));
+        }
+        else if(!positive){
+            return Double.parseDouble(temp.substring((firstLoc + 1), temp.indexOf(")")));
+        }
+        return 0;
 
     }
 
     public double getY(){
-        String temp2 = formula.substring()
+        String temp2 = formula.substring(formula.indexOf("^"));
+        int yLoc = 0;
+        boolean up = true;
+        if(temp2.contains("+")){
+            yLoc = temp2.indexOf("+");
+        }
+        else if(!temp2.contains("+")) {
+            yLoc = temp2.indexOf("-");
+            up = false;
+        }
+        else{
+            return 0;
+        }
+        if(up) {
+            return Double.parseDouble(temp2.substring(yLoc + 1));
+        }
+        else if(!up){
+            return -1 * Double.parseDouble(temp2.substring(yLoc + 1));
+        }else{
+            return 0;
+        }
+
+    }
+    public void present(){
+        System.out.println("The vertex of the formula is (" + getX() + ", " + getY() + ")");
     }
 
 
